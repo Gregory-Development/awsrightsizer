dd1a7bfd8a49c61b3a0a389431ac69822cd3f3fd
Jordan M Gregory <jordan.m.gregory@nasa.gov>
2018-07-19 10:46:20 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='awsrightsizer',
-    version='1.0.0',
+    version='1.0.1',
     packages=['rightsizer'],
     url='https://github.com/gregoryjordanm/awsrightsizer.git',
     license='Apache-2.0',
