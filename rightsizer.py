12d07c220eab0e612e5ac0f365222e4c13284ecd
gregoryjordanm <gregory.jordan.m@gmail.com>
2018-07-30 20:52:36 -0500

    migration statement

--- rightsizer.py
+++
@@ -1,5 +0,0 @@
-from rightsizer.main import main
-
-
-if __name__ == "__main__":
-    main()
--- rightsizer/__init__.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-logging.getLogger('boto3').setLevel(logging.WARNING)
-logging.getLogger('botocore').setLevel(logging.WARNING)
